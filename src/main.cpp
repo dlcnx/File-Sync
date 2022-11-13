a8299f10efab3fdcde68ed5f1f84d298f8b78114
dlcn <microccl@foxmail.com>
2022-11-13 20:06:06 +0800

    [lib]<add> 引入vcpkg, 使用openssl与sion

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include "handler.h"
-
+#include "sion.h"
 using namespace dlcn;
 
 void test1() {
@@ -10,11 +10,19 @@ void test1() {
     cout << ClientController::getFileHash("test.json");
 }
 
+void test2() {
+    string url = "https://baidu.com";
+    auto resp = sion::Fetch(url);
+    std::cout << resp.StrBody() << std::endl;
+    std::cout << resp.GetHeader().Get("content-type") << std::endl;
+}
+
 int main() {
     //判断系统并切换编码
 #if defined(WINNT) || defined(_WIN32) || defined(_WIN64)
     system("chcp 65001");
 #endif
-    test1();
+    // test1();
+    test2();
     return 0;
 }
